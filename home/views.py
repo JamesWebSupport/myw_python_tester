8e327ef6d1fd97a3291b6781eb511d1399e7a9de
JamesWebSupport <52965436+JamesWebSupport@users.noreply.github.com>
2021-01-07 19:34:25 -0500

    jan 7th view-list version add home app home page is worked commit One

--- home/views.py
+++ home/views.py
@@ -1,3 +1,15 @@
+# Create your views here.
 from django.shortcuts import render
 
-# Create your views here.
+from polls.models import Question
+
+
+from django.shortcuts import render
+
+from polls.models import Question
+
+
+def home(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'index.html', context)
